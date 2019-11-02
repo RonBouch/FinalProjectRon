f02593bef1a5dc4e71a18610ad9083bdb9873a60
ronb199447 <ronb199447@gmail.com>
2019-11-02 16:34:39 +0200

    DAL & BAL  Webservise

---
+++ BALproj/BALServices.cs
@@ -0,0 +1,33 @@
+﻿using DALproj;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+//using System.Collections.Generic;
+
+
+namespace BALproj
+{
+    public static class BALServices
+    {
+        public static string Login(string email, string password)
+        {
+         
+            User u = DBServices.Login(email, password);
+            if (u != null)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            return new JavaScriptSerializer().Serialize(u);
+        }
+    }
+
+    
+}
