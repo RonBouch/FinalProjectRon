bbba44e925f3142aeda0fa55ae2b3e55e36b047b
ronb199447 <ronb199447@gmail.com>
2019-11-08 14:42:59 +0200

    d

--- FinalProj/Components/DrawerNavigator.js
+++ FinalProj/Components/DrawerNavigator.js
@@ -13,9 +13,10 @@ import S3 from "../screens/S3.js";
 
 const DNav = createDrawerNavigator(
   {
+    AssociationsList: { screen: AssociationsList },
+
     Contribution: { screen: Contribution },
 
-    AssociationsList: { screen: AssociationsList },
 
     AssociationPage: { screen: AssociationPage },
 
@@ -27,7 +28,7 @@ const DNav = createDrawerNavigator(
     Register: { screen: Register }
   },
   {
-    initialRouteName: "Contribution",
+    initialRouteName: "AssociationsList",
     contentComponent: ContentComponent,
     drawerWidth: Dimensions.get("window").width,
     drawerPosition: "right",
