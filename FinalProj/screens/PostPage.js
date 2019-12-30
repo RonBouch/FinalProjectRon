ef95d26b653c461511716791b4d1d21b0aad2e2b
lerner1208 <lerner1208@gmail.com>
2019-12-30 16:54:43 +0200

    גכנ

--- FinalProj/screens/PostPage.js
+++ FinalProj/screens/PostPage.js
@@ -43,7 +43,7 @@ class PostPage extends Component {
   };
 
   callPhone = () => {
-    Linking.openURL("tel:" + this.state.phone);
+    Linking.openURL("tel:" + this.state.item.UserPhone);
   };
 
   callAdditionalPhone = () => {
@@ -95,6 +95,7 @@ class PostPage extends Component {
                 />
               </TouchableOpacity>
             </View>
+
             {/* <Image
               source={{
                 uri:
@@ -110,21 +111,64 @@ class PostPage extends Component {
               img={this.props.navigation.state.params.item.ItemImg}
               navigation={this.props.navigation}
             />
-            <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+
+            <Text style={{ fontWeight: "bold", fontSize: 24 }}>
               {this.state.item.ItemName}
             </Text>
-            <Text onPress={this.callPhone}>
-              טלפון : <Text> {this.state.item.City}</Text>
-            </Text>
-            <Text>
-              עיר :{" "}
-              <Text style={{ color: "green" }}>
-                {this.state.item.UserPhone}
-              </Text>
-            </Text>
-            ) : ( console.log("No Phone") )}
+
+            <View style={{ alignItems: "flex-start", width: "90%" }}>
+              <Text>פרטי התקשרות:</Text>
+            </View>
+            <View
+              style={{
+                width: "90%",
+                height: 80,
+                alignItems: "flex-start",
+                justifyContent: "space-between",
+                marginTop: 20
+              }}
+            >
+              <View style={{ flexDirection: "row" }}>
+                <Icona
+                  iconStyle={{ marginEnd: "10%" }}
+                  name="user"
+                  type="font-awesome"
+                  color="black"
+                  size={20}
+                />
+                <Text>{this.state.item.UserName}</Text>
+              </View>
+
+              <View style={{ flexDirection: "row" }}>
+                <Icona
+                  iconStyle={{ marginEnd: "10%" }}
+                  name="map-marker"
+                  type="font-awesome"
+                  color="black"
+                  size={20}
+                />
+                <Text>{this.state.item.City}</Text>
+              </View>
+
+              <View style={{ flexDirection: "row" }}>
+                <Icona
+                  iconStyle={{ marginEnd: "10%" }}
+                  name="mobile"
+                  type="font-awesome"
+                  color="black"
+                  size={24}
+                />
+                <Text onPress={this.callPhone} style={{ color: "green" }}>
+                  {this.state.item.UserPhone}
+                </Text>
+              </View>
+            </View>
+
+            <View
+              style={{ borderBottomWidth: 1, width: "90%", margin: 20 }}
+            ></View>
             {this.state.item.ItemAbout != "" ? (
-              <Text style={{ fontSize: 14, padding: 10 }}>
+              <Text style={{ fontSize: 14, marginTop: 30 }}>
                 {this.state.item.ItemAbout}
               </Text>
             ) : (
